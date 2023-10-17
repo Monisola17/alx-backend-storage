d64168aa4f58f83ba7a50b54c3923453b52879b7
Monisola <khairaah@gmail.com>
2023-10-17 15:55:27 -0700

    Done

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,41 +1,31 @@
 #!/usr/bin/env python3
-""" 12. Log stats
+"""log stats from collection
 """
-
-
 from pymongo import MongoClient
 
 
-# MongoDB connection details
-mongo_uri = "mongodb://localhost:27017"
-database_name = "logs"
-collection_name = "nginx"
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-# Connect to MongoDB and access the collection
-client = MongoClient(mongo_uri)
-db = client[database_name]
-collection = db[collection_name]
 
-# Count the total number of logs
-total_logs = collection.count_documents({})
+def log_stats(mongo_collection, option=None):
+    """ script that provides some stats about Nginx logs stored in MongoDB
+    """
+    items = {}
+    if option:
+        value = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {value}")
+        return
 
-# Count the number of logs for each HTTP method
-http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {}
+    result = mongo_collection.count_documents(items)
+    print(f"{result} logs")
+    print("Methods:")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    status_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{status_check} status check")
 
-for method in http_methods:
-    method_count = collection.count_documents({"method": method})
-    method_counts[method] = method_count
 
-# Count the number of logs with method=GET and path=/status
-status_logs = collection.count_documents({"method": "GET", "path": "/status"})
-
-# Display the statistics
-print(f"{total_logs} logs")
-print("Methods:")
-
-for method, count in method_counts.items():
-    print(f"\tmethod {method}: {count}")
-
-print(f"{status_logs} status check")
-
+if __name__ == "__main__":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
