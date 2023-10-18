e9066e1cb6cb87f262b9bc1d5ba8e078b0cdc545
Monisola <khairaah@gmail.com>
2023-10-18 15:00:36 -0700

    Redis basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 """Redis and Python exercise"""
-import redis
 import uuid
-from typing import Union, Callable
+from functools import wraps
+from typing import Callable, Union
+import redis
 
+
+def count_calls(method: Callable) -> Callable:
+    """decorator that takes a single method Callable argument
+    and returns a Callable"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """increments the count for that key every time the method
+        is called and returns the value returned by the original method """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache():
     """Cache class with redis"""
