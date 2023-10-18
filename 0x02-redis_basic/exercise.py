d7e372875b2a370977f2621d0fc1c559854a5895
Monisola <khairaah@gmail.com>
2023-10-18 14:54:53 -0700

    Redis basics

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,26 +5,46 @@ import uuid
 from typing import Union, Callable
 
 
-class Cache:
+class Cache():
+    """Cache class with redis"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Store method
+
+        Args:
+            data (Union[str, bytes, int, float]): Data to be stored
+
+        Returns:
+            str: string
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable[[bytes], any] = None)/
-          -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Callable = None)\
+            -> Union[str, bytes, int, float]:
+        """ Get data from redis and transform it to its python type """
         data = self._redis.get(key)
-        if data is not None and fn is not None:
+        if fn:
             return fn(data)
         return data
 
     def get_str(self, key: str) -> str:
-        return self.get(key, fn=lambda data: data.decode("utf-8"))
+        """ Transform a redis type variable to a str python type """
+        variable = self._redis.get(key)
+        return variable.decode("UTF-8")
 
     def get_int(self, key: str) -> int:
-        return self.get(key, fn=int)
+        """ Transform a redis type variable to a str python type """
+        variable = self._redis.get(key)
+        try:
+            variable = int(variable.decode("UTF-8"))
+        except Exception:
+            variable = 0
+        return variable
